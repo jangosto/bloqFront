daf599e0dde928b055c229e9786d94dfbd1acd07
jangosto <juan.angosto.h@gmail.com>
2015-12-16 00:20:02 +0100

    Added test cover

--- src/AppBundle/Controller/EditorialContentController.php
+++ src/AppBundle/Controller/EditorialContentController.php
@@ -17,9 +17,36 @@ use AppBundle\Form\Type\UserCreationFormType as AdminUserCreationFormType;
 class EditorialContentController extends Controller
 {
     /**
-     * @Route("/{section}/{title}-{date}.html", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
-     * @Route("/{section}/{subsection}/{title}-{date}.html", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
-     * @Route("/{section}/{subsection}/{subsubsection}/{title}-{date}.html", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
+     * @Route("/", name="test_list_editorial_contents")
+     */
+    public function publishedContentsList()
+    {
+        $urlManager = $this->container->get('editor.url.manager');
+        $urls = $urlManager->getAllEnabled();
+
+        $contents = array();
+        foreach ($urls as $url) {
+            if ($url->getCanonical() === true) {
+                $editorialContentManager = $this->container->get('editor.'.$url->getContentType().'.manager');
+                $editorialContent = $editorialContentManager->getById($url->getContentId(), true);
+                $element['url'] = $url->getUrl();
+                $element['type'] = $editorialContent->getType();
+                $element['title'] = $editorialContent->getTitle();
+                $contents[$editorialContent->getType()][] = $element;
+            }
+        }
+
+        return $this->render('contentList.html.twig', array(
+            'user' => $this->getUser(),
+            'contents' => $contents
+        ));
+    }
+
+
+    /**
+     * @Route("/{section}/{title}-{date}.html", name="front_editorial_content_show_section", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
+     * @Route("/{section}/{subsection}/{title}-{date}.html", name="front_editorial_content_show_subsection", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
+     * @Route("/{section}/{subsection}/{subsubsection}/{title}-{date}.html", name="front_editorial_content_show_subsubsection", requirements={"title" = ".+", "date" = "[1-3][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]"})
      */
     public function editorialContentShow(Request $request, $section, $subsection = null, $subsubsection = null, $title, $date)
     {
@@ -34,6 +61,10 @@ class EditorialContentController extends Controller
         $editorialContentClass = $this->container->getParameter("editorial_contents.".$url->getContentType().".model_class");
         $editorialContentManager = $this->container->get('editor.'.$url->getContentType().'.manager');
         $editorialContent = $editorialContentManager->getById($url->getContentId(), true);
+
+        if ($editorialContent->getEnabled !== true) {
+            throw $this->createNotFoundException('Content Not Found');
+        }
 
         if ($url->getCanonical() === true) {
             $canonicalUrl = $url;
